cb15115e2135bd8221c33b239e6931cf4af42898
Mariano Uriel Delgado <mdelgado@munidigital.com>
2022-04-28 11:39:31 -0300

    Update

---
+++ BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by Mariano Uriel Delgado on 15/03/2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+            
+    }
+    
+}
